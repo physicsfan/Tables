f9ddb26dcc3c9f340ee1b95e5fef6c2b9fcd4ba5
Andrew Senchuk <asenchuk@cube.cs.ubc.ca>
2019-12-27 10:01:40 -0800

    initial commit

---
+++ case2/Case2.c
@@ -0,0 +1,20 @@
+Core subshells:
+  1s   2s   2p-  2p
+Peel subshells:
+  3s   3p-  3p
+Nblock =  3
+CSF(s):
+  3s ( 1)  3p-( 1)
+      1/2      1/2
+                  0-
+ *
+  3s ( 1)  3p ( 1)
+      1/2      3/2
+                  1-
+  3s ( 1)  3p-( 1)
+      1/2      1/2
+                  1-
+ *
+  3s ( 1)  3p ( 1)
+      1/2      3/2
+                  2-
